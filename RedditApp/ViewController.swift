0ab76e487ce2a37709e990c0eadd42697b22508c
Fawad Suhail <fawadsuhail@gmail.com>
2020-11-03 18:32:31 -0500

    Initial commit

---
+++ RedditApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RedditApp
+//
+//  Created by Fawad Suhail on 2020-11-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
